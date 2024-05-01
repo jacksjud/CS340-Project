e3998dea2d778406b973234bed8d950126e4089f
jacksjud <137832064+jacksjud@users.noreply.github.com>
2024-04-30 18:09:20 -0700

    Update projectQ.sql

--- projectQ.sql
+++ projectQ.sql
@@ -1,34 +1,16 @@
-create or replace table Investments (
-    investID int not NULL AUTO_INCREMENT unique PRIMARY KEY,
-    stockID int,
-    investorID int,
-    FOREIGN KEY stockID REFERENCES Stocks(stockID),
-    FOREIGN KEY investorID REFERENCES Investors(investorID)
-);
-
 create or replace table Investors (
-    investorID int AUTO_INCREMENT unique not NULL PRIMARY KEY,
+    investorID int unique not NULL AUTO_INCREMENT PRIMARY KEY,
     name varchar(50) not NULL
 );
 
-create or replace table InvestedStocks (
-    investedStockID int not NULL AUTO_INCREMENT PRIMARY KEY,
-    stockID int,
-    investID int,
-    quantity int not NULL,
-    investment decimal(19,2) not NULL
-    FOREIGN KEY (stockID) REFERENCES Stocks(stockID),
-    FOREIGN KEY (investID) REFERENCES Investments(investID)
-);
-
 create or replace table Stocks (
-    stockID int AUTO_INCREMENT not NULL PRIMARY KEY,
+    stockID int unique not NULL AUTO_INCREMENT PRIMARY KEY,
     symbol varchar(256) not NULL,
     companyName varchar(256) not NULL
 );
 
 create or replace table Changes (
-    changeID int not NULL AUTO_INCREMENT unique PRIMARY KEY,
+    changeID int unique not NULL AUTO_INCREMENT PRIMARY KEY,
     stockID int,
     priceOpen decimal (19,2) not NULL,
     priceClose  decimal (19,2) not NULL,
@@ -37,6 +19,27 @@ create or replace table Changes (
     date date not NULL,
     FOREIGN KEY (stockID) REFERENCES Stocks(stockID)
 );
+
+create or replace table Investments (
+    investID int unique not NULL AUTO_INCREMENT PRIMARY KEY,
+    stockID int,
+    investorID int,
+    date date not NULL,
+    FOREIGN KEY stockID REFERENCES Stocks(stockID),
+    FOREIGN KEY investorID REFERENCES Investors(investorID)
+);
+
+create or replace table InvestedStocks (
+    investedStockID int unique not NULL AUTO_INCREMENT PRIMARY KEY,
+    stockID int,
+    investID int,
+    quantity int not NULL,
+    investment decimal(19,2) not NULL
+    FOREIGN KEY (stockID) REFERENCES Stocks(stockID),
+    FOREIGN KEY (investID) REFERENCES Investments(investID)
+);
+
+
 
 -- CREATE OR REPLACE TABLE Invoices (
 --     InvoiceID int NOT NULL AUTO_INCREMENT,
